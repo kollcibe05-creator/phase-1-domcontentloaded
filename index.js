d0441874992c0f3fba0ef0f18c70c2c86743edde
Collins Kibet <kollcibe05@gmail.com>
2025-09-21 13:07:29 +0300

    solved commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function replaceWord() {
+    text = document.querySelector('#text')
+    text.textContent = "This is really cool!"
+    body.append(text)
+} )
+
+
